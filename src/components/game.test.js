e33f7c16549f942b505b4e79735442820494da24
LarissaMorrell <larissa@larissamorrell.com>
2017-09-28 17:16:29 -0400

    fixed test adapter issue. all tests a-okay

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import Game from './game';
-import Adapter from 'enzyme-adapter-react-16';
-import Enzyme from 'enzyme';
-
-Enzyme.configure({ adapter: new Adapter() });
+// import Adapter from 'enzyme-adapter-react-15';
+// import Enzyme from 'enzyme';
+//
+// Enzyme.configure({ adapter: new Adapter() });
 // import List from './list';
 // import {addList} from '../actions';
 
